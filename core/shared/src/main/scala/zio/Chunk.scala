2a13fde2ed5bfc3426087b1f665298470e8da7e8
Ilias Antoniou <helias1990@hotmail.com>
2021-10-31 10:24:13 -0100

    remove endianness

--- core/shared/src/main/scala/zio/Chunk.scala
+++ core/shared/src/main/scala/zio/Chunk.scala
@@ -1098,7 +1098,7 @@ sealed abstract class Chunk[+A] extends ChunkLike[A] { self =>
     }
 
   /** Converts a chunk of longs to a chunk of bits using the specified endianness */
-  final def longsAsBits(endian: ByteOrder)(implicit ev: A <:< Long): Chunk[Boolean] =
+  final def longsAsBits(implicit ev: A <:< Long): Chunk[Boolean] =
     Chunk.BitChunk(Chunk(1, 2), 0, length << 6)
 }
 
